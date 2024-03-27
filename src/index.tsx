36a2151f7a76b4292b21c34ceb96b9ac03fb516b
minickyu <yminick@foxmail.com>
2024-03-28 02:12:46 +0800

    feat: dark model

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './custom-theme.less'; // 引入自定义主题文件
+import ConfigProvider from 'antd/es/config-provider';
+import theme from 'antd/es/theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider theme={{ algorithm: theme.darkAlgorithm }}>
+      <App />
+    </ConfigProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
